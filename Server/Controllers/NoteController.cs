64fe22e932d8739d89f9f0f3b66dda0efef69432
Damian Borowy <tymooteuusz@gmail.com>
2019-10-14 22:49:12 +0200

    Cleaned up the repo and played with ASP.NET and Blazor a bit

---
+++ Server/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlazorNotes.Shared;
+
+namespace BlazorNotes.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Note> Get()
+        {
+            return Enumerable.Range(0, 5).Select(i => new Note
+            {
+                Date = DateTime.Now.Date,
+                Author = "Borowy",
+                Content = "Fooo"
+            }).ToList();
+        }
+    }
+}
---
+++ Shared/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorNotes.Shared
+{
+    public class Note
+    {
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
